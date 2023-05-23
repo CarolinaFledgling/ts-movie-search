ade147c565ead7c6b38c5782d65259f734a77c3c
Karolina Kulinska <karolina.kulinska89@gmail.com>
2023-05-23 19:24:05 +0200

    added read me,

--- script.ts
+++ script.ts
@@ -1,1 +1,27 @@
-const apiKey = process.env.API_KEY
+
+
+interface Config {
+  api: {
+    apiKey: string | undefined;
+    apiUrl: string;
+  };
+}
+
+const config: Config = {
+  api: {
+     // Please note that placing the API key here is not recommended in a production environment. It's advisable to handle it securely on the server-side.
+    apiKey: "",
+    apiUrl: "https://api.themoviedb.org/3/",
+  },
+};
+
+async function search() {
+  const queryString = window.location.search;
+  console.log(queryString);
+}
+
+function init() {
+  search();
+}
+
+document.addEventListener("DOMContentLoaded", init);
